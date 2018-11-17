0e61be639decc8ea696272400a9b0464c2c05cd9
Florin Pățan <florinpatan@gmail.com>
2018-11-17 10:01:14 +0800

    Initial commit

--- intro.go
+++ intro.go
@@ -2,8 +2,7 @@
 // Work: Developer Advocate @ JetBrains (GoLand IDE)
 // Blog: https://blog.jetbrains.com/go
 // Mail: florin@jetbrains.com
-// Gophers Slack: @DLSNIPER (https://invite.slack.golangbridge.org)
+// Gophers Slack: @DLSNIPER (https://invite.slack.golangbridge.org) #goland
 // Sources at: https://github.com/dlsniper/goperftalk/
-
 
 package goperftalk
